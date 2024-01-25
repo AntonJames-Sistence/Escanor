f7febc4c453e1371b0227277647436aa8f167629
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-01-24 20:55:25 -0500

    GenerateGrid function complete

--- _Scripts/GridManager.cs
+++ _Scripts/GridManager.cs
@@ -8,10 +8,15 @@ public class GridManager : MonoBehaviour
 
     [SerializeField] private Tile _tilePrefab;
 
+    void Start() {
+        GenerateGrid();
+    }
+
     void GenerateGrid() {
         for (int x = 0; x < _width; x++) {
             for (int y = 0; y < _height; y++) {
-
+                var spawnedTile = Instantiate(_tilePrefab,new Vector3(x,y),Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
             }
         }
     }
