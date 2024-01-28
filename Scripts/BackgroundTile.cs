3ab6e23e638a16d35836ee0ae37beb90920ba36b
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-01-28 13:39:50 -0500

    Add Initialize to backgroundTile object

--- Scripts/BackgroundTile.cs
+++ Scripts/BackgroundTile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BackgroundTile : MonoBehaviour
 {
+    public GameObject[] elements;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,13 @@ public class BackgroundTile : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Initialize()
+    {
+        int elementToUse = Random.Range(0, elements.Length);
+        GameObject element = Instantiate(elements[elementToUse], transform.position, Quaternion.identity);
+        element.transform.parent = this.transform;
+        element.name = this.gameObject.name;
     }
 }
