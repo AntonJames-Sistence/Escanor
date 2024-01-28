967222bd670df4fc15f38349020b575ece8e144d
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-01-28 17:30:26 -0500

    Add SwapElements logic

--- Scripts/Element.cs
+++ Scripts/Element.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Element : MonoBehaviour
 {
+    public int column;
+    public int row;
+    public int targetX;
+    public int targetY;
+    private Board board;
+    private GameObject neighborElement;
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
     public float swipeAngle;
@@ -11,13 +17,16 @@ public class Element : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        board = FindObjectOfType<Board>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        targetX = column;
+        targetY = row;
     }
 
     private void OnMouseDown()
@@ -34,5 +43,34 @@ public class Element : MonoBehaviour
     void CalculateAngle()
     {
         swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+    }
+
+    void SwapElements()
+    {
+        if(swipeAngle > -45 && swipeAngle <= 45)
+        {
+            //Swap right
+            neighborElement = board.allElements[column + 1, row];
+            neighborElement.GetComponent<Element>().column -= 1;
+            column += 1;
+        } else if(swipeAngle > 45 && swipeAngle <= 135)
+        {
+            //Swap up
+            neighborElement = board.allElements[column, row + 1];
+            neighborElement.GetComponent<Element>().row -= 1;
+            row += 1;
+        } else if(swipeAngle > 135 || swipeAngle <= -135)
+        {
+            //Swap left
+            neighborElement = board.allElements[column + 1, row];
+            neighborElement.GetComponent<Element>().column += 1;
+            column -= 1;
+        } else if(swipeAngle < -45 && swipeAngle >= -135)
+        {
+            //Swap down
+            neighborElement = board.allElements[column, row - 1];
+            neighborElement.GetComponent<Element>().row += 1;
+            row -= 1;
+        }
     }
 }
