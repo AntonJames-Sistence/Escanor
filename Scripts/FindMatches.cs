adf99762b8b1d50a4a96942dece8fe372524b28d
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-02-03 16:47:13 -0500

    Create FindAllMatches coroutine

--- Scripts/FindMatches.cs
+++ Scripts/FindMatches.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class FindMatches : MonoBehaviour
 {
+    private Board board;
+    public List<GameObject> currentMatches = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        board = FindObjectOfType<Board>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FindMatchesCo()
     {
-        
+        yield return new WaitForSeconds(.2f);
+
+        for (int i = 0; i < board.width; i++)
+        {
+            for (int j = 0; j < board.height; j++)
+            {
+                GameObject currentElement = board.allElements[i, j];
+
+                if (currentElement != null)
+                {
+                    if (i > 0 && i < board.width - 1)
+                    {
+                        GameObject leftElement = board.allElements[i - 1, j];
+                        GameObject rightElement = board.allElements[i + 1, j];
+
+                        if (leftElement && rightElement)
+                        {
+                            if (leftElement.tag == currentElement.tag && rightElement.tag == currentElement.tag)
+                            {
+                                leftElement.GetComponent<Element>().isMatched = true;
+                                rightElement.GetComponent<Element>().isMatched = true;
+                                currentElement.GetComponent<Element>().isMatched = true;
+                            }
+                        }
+                    }
+
+                    if (j > 0 && j < board.height - 1)
+                    {
+                        GameObject upElement = board.allElements[i, j + 1];
+                        GameObject downElement = board.allElements[i, j - 1];
+
+                        if (upElement && downElement)
+                        {
+                            if (upElement.tag == currentElement.tag && downElement.tag == currentElement.tag)
+                            {
+                                upElement.GetComponent<Element>().isMatched = true;
+                                downElement.GetComponent<Element>().isMatched = true;
+                                currentElement.GetComponent<Element>().isMatched = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+
 }
