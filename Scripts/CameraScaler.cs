451c5abc284e7a7639dc1cfd068452401d562375
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-02-22 20:27:05 -0500

    Add RepositionCamera method

--- Scripts/CameraScaler.cs
+++ Scripts/CameraScaler.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class CameraScaler : MonoBehaviour
 {
+
+    private Board board;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        board = FindObjectOfType<Board>();
+        if (board != null)
+        {
+            RepositionCamera(board.width, board.height);
+        }
+    }
+
+    void RepositionCamera(float x, float y)
+    {
+        Vector2 tempPosition = new Vector2(Mathf.Round(x/2), Mathf.Round(y/2));
+        transform.position = tempPosition;
     }
 
     // Update is called once per frame
