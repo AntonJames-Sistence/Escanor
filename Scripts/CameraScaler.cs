7a3476cbc06ae2c966eaf8ae30c71d2023383d5e
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-02-22 20:44:30 -0500

    Add padding and orthographicSize, to control size of the screen

--- Scripts/CameraScaler.cs
+++ Scripts/CameraScaler.cs
@@ -6,6 +6,10 @@ public class CameraScaler : MonoBehaviour
 {
 
     private Board board;
+    public float cameraOffset;
+
+    public float aspectRatio = 0.625f;
+    public float padding = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -13,19 +17,23 @@ public class CameraScaler : MonoBehaviour
         board = FindObjectOfType<Board>();
         if (board != null)
         {
-            RepositionCamera(board.width, board.height);
+            RepositionCamera(board.width - 1, board.height - 1);
         }
     }
 
     void RepositionCamera(float x, float y)
     {
-        Vector2 tempPosition = new Vector2(Mathf.Round(x/2), Mathf.Round(y/2));
+        // Adapting camera to size of the board automaticly
+        Vector3 tempPosition = new Vector3(x/2, y/2, cameraOffset);
         transform.position = tempPosition;
+        // Adapting orthographic size (size of the screen)
+        if (board.width >= board.height)
+        {
+            Camera.main.orthographicSize = (board.width / 2 + padding) / aspectRatio;
+        } else 
+        {
+            Camera.main.orthographicSize = board.height / 2 + padding;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
