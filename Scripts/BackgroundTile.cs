36f1187e36c7a82500b2edc238e10b181d7476a6
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-02-26 21:49:01 -0500

    Add color change on removing HP from ice cubes

--- Scripts/BackgroundTile.cs
+++ Scripts/BackgroundTile.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class BackgroundTile : MonoBehaviour
 {
     public int hitPoints;
+    private SpriteRenderer sprite;
 
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
     public void Update()
     {
         if (hitPoints <= 0)
@@ -17,5 +22,16 @@ public class BackgroundTile : MonoBehaviour
     public void TakeDamage(int damage)
     {
         hitPoints -= damage;
+        MakeLighter();
+    }
+
+    // Make ice sprite lighter
+    void MakeLighter()
+    {
+        // Take current color
+        Color color = sprite.color;
+        // Get the color alpha value
+        float newAlpha = color.a * .5f;
+        sprite.color = new Color(color.r, color.g, color.b, newAlpha);
     }
 }
